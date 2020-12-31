1ee41465d1cb5eb2a7e8e38710d36cbe5c94b185
64byte <8byte@naver.com>
2020-12-31 12:44:38 +0900

    Update 11945.cpp

--- boj/11945.cpp
+++ boj/11945.cpp
@@ -44,3 +44,47 @@ int main(void) {
 	cout << "\n";
 	
 }
+
+/* m열을 뒤에서부터  받으면 된다.
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <climits>
+#include <string>
+#include <stack>
+#include <tuple>
+
+using namespace std;
+
+char m[11][11];
+
+int main(void) {
+	long long N, M;
+
+	cin >> N >> M;
+
+	for (int i=0; i<N; ++i)
+	{
+		for (int j=M-1; j>=0; --j)
+		{
+			cin >> m[i][j];
+		}
+	}
+
+	for (int i=0; i<N; ++i)
+	{
+		for (int j=0; j<M; ++j)
+		{
+			cout << m[i][j] << "";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+	
+}
+*/
