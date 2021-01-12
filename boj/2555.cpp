e8a9f4f78d0bd182fb3e66d1fc946a4e500db6b2
64byte <8byte@naver.com>
2021-01-12 22:08:32 +0900

    Create 2555.cpp

---
+++ boj/2555.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <climits>
+#include <map>
+#include <string>
+#include <stack>
+#include <tuple>
+
+using namespace std;
+
+int main(void) {
+	cout << "10/14" << endl;
+}
