6eee2fdb9f171f664b9acc38a27c47e2b50195bb
64byte <8byte@naver.com>
2021-02-25 23:04:19 +0900

    Create 2420.cpp

---
+++ boj/2420.cpp
@@ -0,0 +1,29 @@
+/*
+https://www.acmicpc.net/problem/2420
+*/
+
+#include <iostream>
+#include <utility>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <climits>
+#include <map>
+#include <string>
+#include <stack>
+#include <tuple>
+
+using namespace std;
+
+long long N, M;
+
+int main(void) {
+
+	cin >> N >> M;
+
+	cout << abs(N - M) << endl;
+
+}
