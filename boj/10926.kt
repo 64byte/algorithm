738b41d6c6cd617b4bf1b458bdbcf09d8b97bb4c
64byte <8byte@naver.com>
2021-03-04 21:25:29 +0900

    Create 10926.kt

---
+++ boj/10926.kt
@@ -0,0 +1,14 @@
+import java.math.BigInteger
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+fun main(args: Array<String>) {
+    var sc: Scanner = Scanner(System.`in`)
+
+    var s: String = sc.next()
+
+    println("$s??!")
+
+}
