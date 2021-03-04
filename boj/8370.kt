dceefa419f96e322f410a10e1a23ce86f889101f
64byte <8byte@naver.com>
2021-03-04 21:02:51 +0900

    Create 8370.kt

---
+++ boj/8370.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+
+    var a = sc.nextInt()
+    a *= sc.nextInt()
+    var c = sc.nextInt()
+    c *= sc.nextInt()
+
+    println(a +c)
+
+}
