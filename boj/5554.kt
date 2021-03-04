72cab69a8df83bf6aa837fe8fc9d7334d213a855
64byte <8byte@naver.com>
2021-03-04 20:55:57 +0900

    Create 5554.kt

---
+++ boj/5554.kt
@@ -0,0 +1,15 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val sc: Scanner = Scanner(System.`in`)
+
+    var value: Int = 0
+    var sum: Int = 0
+    for (_1 in 1..4) {
+        value = sc.nextInt()
+        sum += value
+    }
+
+    println(sum/60)
+    println(sum%60)
+}
