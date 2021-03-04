e46cf7fc95577b75418dff96ee3108cecdb54e3c
64byte <8byte@naver.com>
2021-03-04 20:49:50 +0900

    Create 5337.kt

---
+++ boj/5337.kt
@@ -0,0 +1,12 @@
+/*
+https://www.acmicpc.net/problem/5337
+
+코틀린으로 기초 문제를 풀어보자
+
+*/
+
+fun main(args: Array<String>) {
+    println(".  .   .\n" +
+            "|  | _ | _. _ ._ _  _\n" +
+            "|/\\|(/.|(_.(_)[ | )(/.")
+}
